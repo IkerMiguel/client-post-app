de6e4b4759eecf3234e348f8bdae4df850c9ac58
IkerMiguel <ikmiguel03@gmail.com>
2025-05-20 22:03:55 -0500

    Users-table and dashboard

--- src/app/admin-dashboard/pages/user-page/user-page.component.ts
+++ src/app/admin-dashboard/pages/user-page/user-page.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UserTableComponent } from "../../../users/components/user-table/user-table.component";
+import { UserService } from '../../../users/services/user.service';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-user-page',
-  imports: [],
+  imports: [UserTableComponent],
   templateUrl: './user-page.component.html',
   styleUrl: './user-page.component.css'
 })
 export class UserPageComponent {
+  userService = inject(UserService);
 
+  userResource = rxResource({
+    request: () => ({}),
+    loader: () => {
+      return this.userService.getUsers();
+    },
+  });
 }
